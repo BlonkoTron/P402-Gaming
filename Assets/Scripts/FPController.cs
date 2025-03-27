02f2f0ce557c4540134bdb2b1c328a5c0b97b1ee
BlonkoTron <133660863+BlonkoTron@users.noreply.github.com>
2025-03-27 11:11:07 +0100

    FPplayer Can move and look
    look sens and move speed to be determined based on feel later

--- Assets/Scripts/FPController.cs
+++ Assets/Scripts/FPController.cs
@@ -3,25 +3,58 @@ using UnityEngine.InputSystem;
 
 public class FPController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float mouseSensitivity = 2f;
+    [SerializeField] private float moveSpeed = 1f;
 
+    [SerializeField] private Transform cameraTransform;
+
+    private CharacterController cc;
     private Vector3 movement;
-    private Vector3 lookPos;
+    private Vector3 velocity;
+    private float ySpeed;
+
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
 
     private void FixedUpdate()
+    {
+        Move();
+        Look();
+    }
+
+    private void Move()
+    {
+
+        // here we calculate the direction our character should be moving based on the camera position
+        Vector3 direction = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movement.normalized;
+
+        //mash it together with the speed to get the disired movement :)
+        velocity = direction * moveSpeed;
+
+        //Gravity moment
+        ySpeed = Physics.gravity.y * Time.fixedDeltaTime;
+
+        velocity.y = ySpeed;
+
+        //Moving
+        cc.Move(velocity * Time.fixedDeltaTime);
+    }
+
+    private void Look()
     {
 
     }
 
+    #region input
     private void OnLook(InputValue input)
     {
-        lookPos = input.Get<Vector3>();
+        //lookPos = input.Get<Vector2>();
     }
 
     private void OnMove(InputValue input)
     {
         movement = input.Get<Vector3>();
     }
-
+    #endregion
 }
