1956a56aa74218a6caf3c120635c29dfda5d69de
BlonkoTron <133660863+BlonkoTron@users.noreply.github.com>
2025-04-07 14:17:43 +0200

    camera fix

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
 
     public CinemachineCamera MainCam;
 
+    [SerializeField] private CinemachineCamera npcCam;
+
     public CinemachineCamera activeCam;
     private void Awake()
     {
@@ -26,6 +28,8 @@ public class CameraController : MonoBehaviour
     }
     public void SetToMainCam()
     {
+
+        npcCam.Priority = MainCam.Priority - 1;
         MainCam.Prioritize();
         activeCam = MainCam;
     }
@@ -35,9 +39,13 @@ public class CameraController : MonoBehaviour
         activeCam = cam;
     }
 
-    public void LookAtMe(Vector3 pos)
+    public void SetToNpcCam(Transform target)
     {
-        
+        npcCam.LookAt = target;
+        npcCam.Priority = MainCam.Priority + 1;
+        npcCam.Prioritize();
+        activeCam = npcCam;
+
     }
 
 }
