5aed78ee7ea8dc0f0725931d4466474a22d569c6
oscarbagger <oscarbagger@live.dk>
2025-04-10 10:32:43 +0200

    bottlematerial reset fill value on quit

--- Assets/Scripts/BottleFill.cs
+++ Assets/Scripts/BottleFill.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class BottleFill : MonoBehaviour
 {
-    [SerializeField] private float minValue = 0.5f;
+    [SerializeField] private float minValue = 0.49f;
     [SerializeField] private float maxValue = 0.65f;
+
+    private float startValue;
 
     [SerializeField] private Material bottleFillMat;
     void Start()
     {
+        startValue = bottleFillMat.GetFloat("_Fill");
         DrinkingController.Instance.OnDrink += UpdateBottleFill;
         UpdateBottleFill();
     }
@@ -22,6 +25,10 @@ public class BottleFill : MonoBehaviour
         var newFill=minValue+range* DrinkingController.Instance.bloodAlcoholContent;
         // bottleFillMat - something 
         bottleFillMat.SetFloat("_Fill",newFill);
+    }
+    private void OnApplicationQuit()
+    {
+        bottleFillMat.SetFloat("_Fill", startValue);
     }
 
 
