c84f21ef81292dc6caac125d18262aac55ee0e00
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2025-04-14 14:16:28 +0200

    updat

--- Assets/BenjaminLydSjov/NPC_Soundmanager.cs
+++ Assets/BenjaminLydSjov/NPC_Soundmanager.cs
@@ -8,6 +8,7 @@ public class NPC_Soundmanager : MonoBehaviour
     [SerializeField] private EventReference VoiceLine_3;
     [SerializeField] private EventReference VoiceLine_4;
     [SerializeField] private EventReference VoiceLine_5;
+    [SerializeField] private EventReference Achievement;
 
     public static NPC_Soundmanager Instance_sound;
     InkDialogue inkDialogues;
@@ -16,6 +17,7 @@ public class NPC_Soundmanager : MonoBehaviour
     public string NPC_Dialog3;
     public string NPC_Dialog4;
     public string NPC_Dialog5;
+    public string AchievementName;
     public void storys()
     {
         inkDialogues = GetComponent<InkDialogue>();
@@ -46,6 +48,11 @@ public class NPC_Soundmanager : MonoBehaviour
         {
             Debug.Log(soundName5);
             Audiomanager.instance.PlayOneShot(VoiceLine_5, transform.position);
+        });
+        inkDialogues.story.BindExternalFunction(AchievementName, (string Achievements) =>
+        {
+            Debug.Log(Achievements);
+            Audiomanager.instance.PlayOneShot(Achievement, transform.position);
         });
     }
 }
