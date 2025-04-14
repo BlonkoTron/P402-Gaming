556378eceb60461c46a8d3615ceea50be2ee6d52
oscarbagger <oscarbagger@live.dk>
2025-04-14 11:11:36 +0200

    sceneTransitioner

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,33 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private float transitionTimer = 1;
-    [SerializeField] private Image transitionPanel;
-    [SerializeField] private AnimationCurve fadeCurve;
     public void StartGame()
     {
         AchievementController.Instance.ResetAchievements();
-        StartCoroutine(SceneTransition(transitionTimer));
+        SceneTransititoner.Instance.NextSceneTransition();
     }
     public void QuitGame()
     {
         Application.Quit();
     } 
-    private IEnumerator SceneTransition(float timer)
-    {
-        float journey = 0f;
-        while (journey <= timer)
-        {
-            journey = journey + Time.deltaTime;
-            float percent = Mathf.Clamp01(journey / timer);
-            // fade in panel over time
-            var fade = fadeCurve.Evaluate(percent);
-            transitionPanel.color = new Color(transitionPanel.color.r, transitionPanel.color.g, transitionPanel.color.b, fade);
-            yield return null;
-        }
-        if (journey > timer)
-        {
-            SceneManager.LoadScene(1, LoadSceneMode.Single);
-        }
-    }
 }
