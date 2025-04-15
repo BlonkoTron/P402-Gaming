bcb507b92d25634790af5086021c25ca58629763
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2025-04-15 14:16:00 +0200

    updatedsound

--- Assets/BenjaminLydSjov/NPC_Soundmanager.cs
+++ Assets/BenjaminLydSjov/NPC_Soundmanager.cs
@@ -1,13 +1,22 @@
+using FMOD;
+using FMOD.Studio;
 using FMODUnity;
 using UnityEngine;
 
 public class NPC_Soundmanager : MonoBehaviour
 {
-    [SerializeField] private EventReference VoiceLine_1;
-    [SerializeField] private EventReference VoiceLine_2;
-    [SerializeField] private EventReference VoiceLine_3;
-    [SerializeField] private EventReference VoiceLine_4;
-    [SerializeField] private EventReference VoiceLine_5;
+
+    private EventInstance VoiceLine_1;
+    private EventInstance VoiceLine_2;
+    private EventInstance VoiceLine_3;
+    private EventInstance VoiceLine_4;
+    private EventInstance VoiceLine_5;
+
+    [SerializeField] private EventReference AudioLine_1;
+    [SerializeField] private EventReference AudioLine_2;
+    [SerializeField] private EventReference AudioLine_3;
+    [SerializeField] private EventReference AudioLine_4;
+    [SerializeField] private EventReference AudioLine_5;
 
     public static NPC_Soundmanager Instance_sound;
     InkDialogue inkDialogues;
@@ -21,31 +30,28 @@ public class NPC_Soundmanager : MonoBehaviour
         inkDialogues = GetComponent<InkDialogue>();
         inkDialogues.story.BindExternalFunction(NPC_Dialog1, (string soundName) =>
         {
-            //Audiomanager.instance.PlayOneShot(VoiceLine_1, transform.position);
+            VoiceLine_1 = Audiomanager.instance.PlaySound(AudioLine_1, transform.position);
         });
 
         inkDialogues.story.BindExternalFunction(NPC_Dialog2, (string soundName2) =>
         {
-            Debug.Log(soundName2);
-            //Audiomanager.instance.PlayOneShot(VoiceLine_2, transform.position);
+            VoiceLine_2 = Audiomanager.instance.PlaySound(AudioLine_2, transform.position);
         });
 
         inkDialogues.story.BindExternalFunction(NPC_Dialog3, (string soundName3) =>
         {
-            Debug.Log(soundName3);
-            //Audiomanager.instance.PlayOneShot(VoiceLine_3, transform.position);
+            VoiceLine_3 = Audiomanager.instance.PlaySound(AudioLine_3, transform.position);
         });
 
         inkDialogues.story.BindExternalFunction(NPC_Dialog4, (string soundName4) =>
         {
-            Debug.Log(soundName4);
-            //Audiomanager.instance.PlayOneShot(VoiceLine_4, transform.position);
+            VoiceLine_4 = Audiomanager.instance.PlaySound(AudioLine_4, transform.position);
         });
 
         inkDialogues.story.BindExternalFunction(NPC_Dialog5, (string soundName5) =>
         {
-            Debug.Log(soundName5);
-            //Audiomanager.instance.PlayOneShot(VoiceLine_5, transform.position);
+            //Debug.Log(soundName5);
+            VoiceLine_5 = Audiomanager.instance.PlaySound(AudioLine_5, transform.position);
         });
     }
 }
--- Assets/Scripts/Dialogue/InkDialogue.cs
+++ Assets/Scripts/Dialogue/InkDialogue.cs
@@ -9,7 +9,6 @@ using UnityEngine.Events;
 // This is a super bare bones example of how to play and display a ink story in Unity.
 public class InkDialogue : MonoBehaviour
 {
-	Soundawsom Soundmanager;
 	public static event Action<Story> OnCreateStory;
 
 	public UnityEvent OnStartStory;
