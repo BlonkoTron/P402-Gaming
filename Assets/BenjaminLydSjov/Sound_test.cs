feca7f542aa371d5f007b183f1341fe0de0ee3e0
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2025-04-15 12:44:17 +0200

    testetes

--- Assets/BenjaminLydSjov/Sound_test.cs
+++ Assets/BenjaminLydSjov/Sound_test.cs
@@ -7,7 +7,19 @@ public class Sound_test : MonoBehaviour
     EventInstance musicInstance;
     [SerializeField] private EventReference SFXBALL;
     [SerializeField] private EventReference SFXBALL2;
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Audio1();
+        }
 
+
+        if (Input.GetKeyDown("c"))
+        {
+            Audio2();
+        }
+    }
     public void Audio1()
     {
         Audiomanager.instance.PlayOneShot(SFXBALL, transform.position);
