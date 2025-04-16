8024adbcd4e9f8a5711a196c8a18f24d6e248e75
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2025-04-16 10:54:25 +0200

    lydsjov

--- Assets/BenjaminLydSjov/RandomAmbience.cs
+++ Assets/BenjaminLydSjov/RandomAmbience.cs
@@ -15,14 +15,21 @@ public class RandomAmbience : MonoBehaviour
     private int RandomINT4 = 0;
     private int RandomINT5 = 0;
 
-    // Update is called once per frame
+    // Update is called once per frame NO SHIT SHERLOCK
     void Update()
     {
-        RandomINT1= Random.Range(0, 200);
+        RandomINT1= Random.Range(0, 1000);
         print(RandomINT1);
         if (RandomINT1 == 69)
         {
             soundInstance = Audiomanager.instance.PlaySound(Sound1, transform.position);
         }
+
+        //RandomINT2 = Random.Range(0, 1000);
+        //print(RandomINT2);
+        //if (RandomINT2 == 69)
+        //{
+        //    soundInstance2 = Audiomanager.instance.PlaySound(Sound2, transform.position);
+        //}
     }
 }
