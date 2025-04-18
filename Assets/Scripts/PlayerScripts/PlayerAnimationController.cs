421f83a4889982c9660afbea4072fb976d082239
BlonkoTron <133660863+BlonkoTron@users.noreply.github.com>
2025-04-18 12:58:22 +0200

    g7uiergvi

---
+++ Assets/Scripts/PlayerScripts/PlayerAnimationController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Splines.Interpolators;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+
+    private FPController FP;
+    private Animator animator;
+
+    private float movementValue;
+    private float lerpT;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        FP = GetComponent<FPController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FP.movement != Vector3.zero)
+        {
+          
+            movementValue = Mathf.Lerp(0, FP.movement.magnitude, lerpT);
+
+            lerpT += 1.5f * Time.deltaTime;
+        }
+        else 
+        {
+            if (movementValue > 0)
+            {
+                movementValue -= 1.5f * Time.deltaTime;
+                
+            }
+
+            if (lerpT > 0)
+            {
+                lerpT -= 1.5f * Time.deltaTime;
+            }
+        }
+
+
+        animator.SetFloat("movement", movementValue);
+    }
+}
